8b2329aecb63654ab8ec104a1cfa98dba4888a6f
c-marc <mcar176@gmail.com>
2021-04-01 17:21:40 +0200

    Clean

--- .eleventy.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (eleventy) {
-
-  // Add related posts
-  eleventy.addLiquidShortcode("related", (title, url) => `
-  <a href="${url}" class="related">${title}</a>
-  `);
-
-};
